87ed48ac66f95ef2a6631ea1420c25ffd7dbe6cd
Sohyun <164127801+parksohyunnn@users.noreply.github.com>
2024-05-21 10:54:33 +0900

    섹션 컴퍼넌트 구조화하기

--- src/App.js
+++ src/App.js
@@ -13,23 +13,30 @@ import PremierPro from './pages/PremierPro'
 import AfterEffects from './pages/AfterEffects'
 import Cinema4d from './pages/Cinema4d'
 import Not from './pages/Not'
+import Header from './components/section/Header'
+import Main from './components/section/Main'
+import Footer from './components/section/Footer'
 
 const App = () => {
   return (
     <BrowserRouter>
-      <Routes>
-        <Route path='/' element={<Home />} />
-        <Route path='/recommend' element={<Recommend />} />
-        <Route path='/popularity' element={<Popularity />} />
-        <Route path='/website' element={<Website />} />
-        <Route path='/photoshop' element={<Photoshop />} />
-        <Route path='/illustration' element={<Illustration />} />
-        <Route path='/figma' element={<Figma />} />
-        <Route path='/premierPro' element={<PremierPro />} />
-        <Route path='/afterEffects' element={<AfterEffects />} />
-        <Route path='/cinema4d' element={<Cinema4d />} />
-        <Route path='/*' element={<Not />} />
-      </Routes>
+      <Header />
+      <Main>
+        <Routes>
+          <Route path='/' element={<Home />} />
+          <Route path='/recommend' element={<Recommend />} />
+          <Route path='/popularity' element={<Popularity />} />
+          <Route path='/website' element={<Website />} />
+          <Route path='/photoshop' element={<Photoshop />} />
+          <Route path='/illustration' element={<Illustration />} />
+          <Route path='/figma' element={<Figma />} />
+          <Route path='/premierPro' element={<PremierPro />} />
+          <Route path='/afterEffects' element={<AfterEffects />} />
+          <Route path='/cinema4d' element={<Cinema4d />} />
+          <Route path='/*' element={<Not />} />
+        </Routes>
+      </Main>
+      <Footer />
     </BrowserRouter>
   )
 }
